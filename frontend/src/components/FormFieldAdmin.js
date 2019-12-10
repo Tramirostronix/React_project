8b22fa173e69d2bcea57c96a97f19b6e8b1c453d
Tramirostronix <gaetan1710@hotmail.fr>
2019-12-10 14:05:54 +0100

    affichageusersdone

--- frontend/src/components/FormFieldAdmin.js
+++ frontend/src/components/FormFieldAdmin.js
@@ -6,22 +6,16 @@ import axios from "axios"
 class FormFieldAdmin extends React.Component {
     constructor(props) {
         super(props); 
-        this.state = {
-            userID: "", 
+        this.state = { 
             location:"", 
             personsInHouse:"", 
              houseSize:""
         }
-        this.handleUserID=this.handleUserID.bind(this)
         this.handleLocation=this.handleLocation.bind(this)
         this.handlePersonsInHouse=this.handlePersonsInHouse.bind(this)
         this.handleHouseSize=this.handleHouseSize.bind(this)
         this.handleSubmit=this.handleSubmit.bind(this)
        
-    }
-
-    handleUserID(event) {
-        this.setState({userID: event.target.value})
     }
 
     handleLocation(event) {
@@ -41,7 +35,6 @@ class FormFieldAdmin extends React.Component {
         event.preventDefault()
         
         let users = {
-            userID: this.state.userID,
             location: this.state.location,
             personsInHouse: this.state.personsInHouse, 
             houseSize: this.state.houseSize
@@ -49,34 +42,30 @@ class FormFieldAdmin extends React.Component {
         console.log(users);
         axios.post("http://localhost:3030/users/add", users)
 
-    this.setState({ userID: '', location: '', personsInHouse:"", houseSize:"" })
+    this.setState({location: '', personsInHouse:"", houseSize:"" })
 
     }
 
     render() {
         return(
-            //le user id est a creer automatiquement apres 
+            //le user id est a creer automatiquement par la BDD 
             <div>
                 <Container className="my-container">   
                         <form onSubmit={this.handleSubmit}>
                             <Row className="justify-content-center add">
                                 <h1>Ajouter un Utilisateur</h1>
                             </Row>  
-                            
-                            <Row className="justify-content-center nom_objet">
-                                 <input id="center" type="text" placeholder="User ID" value={this.state.userID} onChange={this.handleUserID} required /> 
-                            </Row>
 
                             <Row className="justify-content-center nom_objet">
                                 <input id="center"  type="text" placeholder="Location" value={this.state.location} onChange={this.handleLocation} required /> 
                             </Row>
 
                             <Row className="justify-content-center nom_objet">
-                                <input id="center"  type="text" placeholder="Number person in the House" value={this.state.personsInHouse} onChange={this.handlePersonsInHouse} required /> 
+                                <input id="center"  type="text" placeholder="personInHouse" value={this.state.personsInHouse} onChange={this.handlePersonsInHouse} required /> 
                             </Row>
 
                             <Row className="justify-content-center nom_objet">
-                                <input id="center"  type="text" placeholder="Size of the House" value={this.state.houseSize} onChange={this.handleHouseSize} required /> 
+                                <input id="center"  type="text" placeholder="SizeHouse" value={this.state.houseSize} onChange={this.handleHouseSize} required /> 
                             </Row>
 
                             <Row className="justify-content-center form_login">
--- frontend/src/components/ShowUsers.js
+++ frontend/src/components/ShowUsers.js
@@ -1,18 +1,12 @@
 import React from "react"
 import axios from "axios"
+
+//source : 
+//https://www.youtube.com/watch?v=hzLDsxPGctY
+
 //pour afficher les utilisateurs
 
-import DataUsers from "./DataUsers"
 import { Table } from "react-bootstrap"
-
-const Users = props => (
-    <tr>
-        <td>{this.props.userID}</td>
-        <td>{this.props.location}</td>
-        <td>{this.props.personsInHouse}</td>
-        <td>{this.props.houseSize}</td>
-    </tr>
-)
 
 
 class ShowUsers extends React.Component {
@@ -44,21 +38,13 @@ class ShowUsers extends React.Component {
         return (
             <div>
                 <h1>Liste des Utilisateurs</h1>
-                <Table striped bordered hover>
-                    <thead className="thead-dark">
-                        <tr>
-                            <td>userID</td>
-                            <td>Location</td>
-                            <td>PersonsInHouse</td>
-                            <td>sizeHouse</td>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-
-                    </tbody>
-
-                </Table>
+                <ul>
+                    {this.state.users.map(user => (
+                        <li key={user._id}>
+                           Id: {user._id} | location : {user.location} | PersonsInHouse ; {user.personsInHouse}| HouseSize : {user.houseSize} 
+                        </li>  
+                    ))}
+                </ul>
 
             </div>
         )
