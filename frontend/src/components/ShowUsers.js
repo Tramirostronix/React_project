b066ea037855f3602e63d813d7d30fb1ae87a7be
Tramirostronix <gaetan1710@hotmail.fr>
2019-12-10 14:48:22 +0100

    showusertotaldone

--- frontend/src/components/ShowUsers.js
+++ frontend/src/components/ShowUsers.js
@@ -1,12 +1,13 @@
 import React from "react"
 import axios from "axios"
+import { Table, Container, Row, Col } from "react-bootstrap"
 
 //source : 
 //https://www.youtube.com/watch?v=hzLDsxPGctY
 
 //pour afficher les utilisateurs
 
-import { Table } from "react-bootstrap"
+
 
 
 class ShowUsers extends React.Component {
@@ -36,17 +37,35 @@ class ShowUsers extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>Liste des Utilisateurs</h1>
-                <ul>
-                    {this.state.users.map(user => (
-                        <li key={user._id}>
-                           Id: {user._id} | location : {user.location} | PersonsInHouse ; {user.personsInHouse}| HouseSize : {user.houseSize} 
-                        </li>  
-                    ))}
-                </ul>
 
+            <div className="table-responsive">
+                <Container className="my-container">
+                    <Row className="justify-content-center">
+                        <h1>Liste des Utilisateurs</h1>
+                    </Row>
+                        <Table striped bordered hover variant="dark">
+                            <thead>
+                                <tr>
+                                    <th>userId</th>
+                                    <th>Location</th>
+                                    <th>PersonsInHouse</th>
+                                    <th>HouseSize</th>
+                                </tr>
+                            </thead>
+                            {this.state.users.map(user => (
+                                <tbody key={user._id}>
+                                    <tr>
+                                        <td>{user._id}</td>
+                                        <td>{user.location}</td>
+                                        <td>{user.personsInHouse}</td>
+                                        <td>{user.houseSize}</td>
+                                    </tr>
+                                </tbody>
+                            ))}
+                        </Table>
+                </Container>
             </div>
+
         )
     }
 }
